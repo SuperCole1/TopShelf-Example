0614a89e0f9956f1395ba357918750619bfacd77
Cole Francis <SuperCole1@users.noreply.github.com>
2018-02-08 17:41:28 -0600

    Example Windows Service using TopShelf
    This is an example of the Windows Service I created using a C# Console Application and the TopShelf NuGet package.

---
+++ TopShelfCWS/TopShelfCWS/NameMeAnything.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TopShelfCWS
+{
+    public class NameMeAnything : ServiceControl
+    {
+        private System.Timers.Timer _timer = new System.Timers.Timer();
+
+        public bool Start(HostControl hostControl)
+        {
+            Console.WriteLine("The Service was Started!");
+
+            // Do something custom here.
+            _timer.Interval = 1000;
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+
+            return true;
+        }
+
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("Thank you for spending this second of your life reading my article.");
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Console.WriteLine("The Service was Stopped!");
+            _timer.Stop();
+            return true;
+        }
+    }
+}
---
+++ TopShelfCWS/TopShelfCWS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TopShelfCWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(host =>
+            {
+                host.SetServiceName("MyCustomerService");
+                host.SetDisplayName("My Customer Service");
+                host.SetDescription("This is my custom service.");
+                host.StartAutomatically();
+
+                host.Service<NameMeAnything>();
+            });
+        }
+    }
+}
